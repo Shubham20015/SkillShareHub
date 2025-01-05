70edc0ebc8d28149c3481539cfa92d62fbf4ec1d
shubham20015 <shubhamjain310@yahoo.in>
2025-01-05 19:17:28 +0530

    Initial commit

---
+++ src/test/java/com/api/skillShare/SkillShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.skillShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
