45571296acf0198944b5b650facd793dd8d3bae5
shubham20015 <shubhamjain310@yahoo.in>
2025-01-13 15:34:11 +0530

    Integrate eureka service registry

--- src/main/java/com/api/skillShare/SkillShareApplication.java
+++ src/main/java/com/api/skillShare/SkillShareApplication.java
@@ -1,11 +1,11 @@
 package com.api.skillShare;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@OpenAPIDefinition
+@EnableDiscoveryClient
 public class SkillShareApplication {
 
 	public static void main(String[] args) {
